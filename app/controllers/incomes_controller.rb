24474457da655362565b43276c1785da5a0d7e1e
silverskg <chokichoki_gm@i.softbank.jp>
2020-05-06 18:10:40 +0900

    view_[index,new,edit]実装

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -1,14 +1,18 @@
 class IncomesController < ApplicationController
   def index
+    @incomes = Income.order(created_at: :asc)
   end
 
   def show
+    @income = Income.find(params[:id])
   end
 
   def new
+    @income = Income.new()
   end
 
   def edit
+    @income = Income.find(params[:id])
   end
 
   def create
