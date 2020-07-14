58a375fa047ae816b35d536ac2b3f7bdc3ffc02e
silverskg <chokichoki_gm@i.softbank.jp>
2020-07-14 16:34:02 +0900

    ゲストログイン配置

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,8 @@
+class Users::SessionsController < Devise::SessionsController
+  def new_guest
+    user = User.guest
+    sign_in user
+    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました'
+  end
+
+end
