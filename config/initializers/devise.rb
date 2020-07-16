5713a9930a8341e31c7a135b03351bec45103753
silverskg <chokichoki_gm@i.softbank.jp>
2020-07-16 21:21:33 +0900

    スペルミス

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -25,7 +25,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   # config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
-  mailer_sender = 'Ikeda_kakebo<noreply@examole.com>'
+  config.mailer_sender = 'Ikeda_kakebo<noreply@examole.com>'
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
